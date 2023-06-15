c00dd2700cc35b2f3131b1d664882caab5b021b0
ilhamfardiansyah <ilhamfardiansyah97@gmail.com>
2023-06-15 13:50:48 +0700

    Upate sidebar

--- resources/views/partials/navbar_superadmin.blade.php
+++ resources/views/partials/navbar_superadmin.blade.php
@@ -105,7 +105,7 @@
                         href="{{ url('dashboard/addimage') }}" role="button" aria-expanded="false">
                         <div class="d-flex align-items-center"><span class="nav-link-icon"><span
                                     class="fas fa-images"></span></span><span class="nav-link-text ps-1">
-                                Manage User</span>
+                                Manage Aplication</span>
                         </div>
                     </a>
 
