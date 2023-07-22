1df78b444be7147461c8a32697f642cda06f7e00
Ilham Fardianysah <93816433+Ilhamfardiansyah@users.noreply.github.com>
2023-07-22 11:59:36 +0700

    Update wxport excel

--- resources/views/Account/Search/Export.blade.php
+++ resources/views/Account/Search/Export.blade.php
@@ -1,125 +1,105 @@
 @extends('layouts.main')
 
-
 @section('content')
-    <div class="card mb-3">
-        <div class="bg-holder d-none d-lg-block bg-card" style="background-image:url(../../../assets/img/corner-4.png);">
-        </div>
-        <!--/.bg-holder-->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
 
-        <div class="card-body position-relative">
-            <div class="row">
-                <div class="col-lg-8">
-                    <h3>Export Idesign</h3>
-                    <p class="mb-0"></p><a class="btn btn-link btn-sm ps-0 mt-2"
-                        href="https://getbootstrap.com/docs/5.1/forms/layout" target="_blank"></a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="card mb-3">
-        <div class="card-body bg-light">
-            <div class="tab-content">
-                <form action="/dashboard/scan/" method="GET">
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-form-nama">Search anggota masukan no passring</label>
-                        <input class="form-control @error('nopassring') is-invalid @enderror" id="nopassring"
-                            name="nopassring" type="text" placeholder="Search anggota masukan no passring"
-                            value="{{ old('nopassring') }}" autofocus autocomplete />
-                        @error('nopassring')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                <div class="card mb-3">
+                    <div class="bg-holder d-none d-lg-block bg-card"
+                        style="background-image:url(../../../assets/img/corner-4.png);">
                     </div>
-                </form>
-            </div>
-        </div>
-    </div>
+                    <!--/.bg-holder-->
 
-    <div class="card mb-3">
-        <div class="bg-holder d-none d-lg-block bg-card" style="background-image:url(../../../assets/img/corner-4.png);">
-        </div>
-        <!--/.bg-holder-->
-        <div class="card-body position-relative">
-            <div class="row">
-                <div class="col-lg-8">
-                    <h3>Export Idesign</h3>
-                    <p class="mb-0"></p><a class="btn btn-link btn-sm ps-0 mt-2"
-                        href="https://getbootstrap.com/docs/5.1/forms/layout" target="_blank"></a>
+                    <div class="card-body position-relative">
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <h3>Manage Anggota</h3>
+                                <p class="mb-0"></p><a class="btn btn-link btn-sm ps-0 mt-2"
+                                    href="https://getbootstrap.com/docs/5.1/forms/layout" target="_blank"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                    <table id="example" class="display nowrap" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Tiger Nixon</td>
-                                <td>System Architect</td>
-                                <td>Edinburgh</td>
-                                <td>61</td>
-                                <td>2011-04-25</td>
-                                <td>$320,800</td>
-                            </tr>
-                            <tr>
-                                <td>Michael Bruce</td>
-                                <td>Javascript Developer</td>
-                                <td>Singapore</td>
-                                <td>29</td>
-                                <td>2011-06-27</td>
-                                <td>$183,000</td>
-                            </tr>
-                            <tr>
-                                <td>Donna Snider</td>
-                                <td>Customer Support</td>
-                                <td>New York</td>
-                                <td>27</td>
-                                <td>2011-01-25</td>
-                                <td>$112,000</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+                <div class="card mb-3">
+                    <div class="card-body bg-light">
+                        <div class="tab-content">
+                            <form action="/dashboard/scan/" method="GET">
+                                <div class="mb-3">
+                                    <label class="form-label" for="basic-form-nama">Scan No Pass Ring</label>
+                                    <input class="form-control @error('nopassring') is-invalid @enderror" id="nopassring"
+                                        name="nopassring" type="text" placeholder="Scan No Pass Ring"
+                                        value="{{ old('nopassring') }}" autofocus autocomplete />
+                                    @error('nopassring')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
 
-                    <script>
-                        $(document).ready(function() {
-                            $('#example').DataTable({
-                                dom: 'Bfrtip',
-                                buttons: [
-                                    'copy', 'csv', 'excel', 'pdf', 'print'
-                                ]
-                            });
-                        });
-                    </script>
+                <div class="card mb-3">
+                    <div class="card-body bg-light">
+                        <div class="tab-content">
+                            <div class="tab-pane preview-tab-pane active" role="tabpanel"
+                                aria-labelledby="tab-dom-d4ebf6c5-74b4-4308-8c64-cda718c9b324"
+                                id="dom-d4ebf6c5-74b4-4308-8c64-cda718c9b324">
+                                @include('sweetalert::alert')
+                            </div>
+                            <div class="table-responsive scrollbar">
 
+                                <table id="example" class="display nowrap" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama Anggota</th>
+                                            <th>No Passring</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data as $datas)
+                                            <tr>
+                                                <td>{{ $datas->nama }}</td>
+                                                <td>{{ $datas->nopassring }}</td>
+                                                <td class="text-center">
+                                                    <a class="btn btn-primary btn-sm me-2 mb-1" href="{{ $datas->id }}">
+                                                        <i class='fa fa-print' style='color: rgb(255, 255, 255)'></i></a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'excel'
+                ]
+            });
+        });
+    </script>
 @endsection
-
-
-
-<script>
-    var input = document.querySelector('#input1');
-    input.addEventListener('keyup', function() {
-        alert('test')
-    })
-</script>
--- resources/views/partials/navbar_superadmin.blade.php
+++ resources/views/partials/navbar_superadmin.blade.php
@@ -42,13 +42,13 @@
                         </div>
                     </a>
 
-                    {{-- <a class="nav-link nav-link {{ Request::is('dashboard/export') ? 'active' : '' }}"
+                    <a class="nav-link nav-link {{ Request::is('dashboard/export') ? 'active' : '' }}"
                         href="{{ route('export') }}" role="button" aria-expanded="false">
                         <div class="d-flex align-items-center"><span class="nav-link-icon"><span
-                                    class="fas fa-palette"></span></span><span class="nav-link-text ps-1">Export I
+                                    class="fas fa-palette"></span></span><span class="nav-link-text ps-1">Export To i
                                 Design</span>
                         </div>
-                    </a> --}}
+                    </a>
 
                     {{-- Anggota --}}
                     <div class="row navbar-vertical-label-wrapper mt-3 mb-2">
